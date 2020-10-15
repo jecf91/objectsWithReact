bd25bfaf5c7838b826818f163a7e65d7d137392a
unknown <joao.e.c.f@gmail.com>
2020-10-15 21:31:00 +0100

    chore: studies on object iteration in JavaScript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const raptors = {
+  aaa111: {
+    name: "Kawhi Leonard"
+  },
+  bbb222: {
+    name: "Kyle Lowry"
+  },
+  ccc333: {
+    name: "Spicy P"
+  }
+};
+
+
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <ul>
+      {//Object.key returns an array with the keys of the object
+      //then we can map the keys, using each key to access the correct attribute
+
+      Object.keys(raptors).map(objectKey => (
+      <li key={objectKey}>{raptors[objectKey].name}</li>
+      ))}
+    </ul>
+    
+    <ul>
+      {//Object.values returns an array with the values of the object
+      //then we can also map the values and access the attribute we want to display
+      //this solution has some issues, because we may not have an id attribute
+      Object.values(raptors).map(( value, index ) => (
+        <li key={index}>{value.name}</li>
+      ))}
+    </ul>
+
+    <ul>
+      {//Object.entries returns a tuple, wich the first element is the key and the second a value
+      //this way we can easily access both the key and the value, being a more flexible approach
+      Object.entries(raptors).map(([objectKey,objectValues]) => (
+        <li key={objectKey}>{objectValues.name}</li>
+      ))
+      }
+    </ul>
+  </>
   );
 }
 
